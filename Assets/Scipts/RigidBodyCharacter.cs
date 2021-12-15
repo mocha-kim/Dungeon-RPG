d099f01a9336667f82106471533634ac3076485a
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-12-15 17:43:28 +0900

    [add] controller character

---
+++ Assets/Scipts/RigidBodyCharacter.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacter : MonoBehaviour
+{
+    #region Variables
+
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float dashDistance = 5f;
+
+    public LayerMask groundLayerMask;
+    public float groundCheckDistance = 0.3f;
+
+    private bool isGrounded = false;
+
+    private Rigidbody rigidbody;
+    private Vector3 inputDirection = Vector3.zero;
+
+    #endregion Variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGroundStatus();
+
+        // Get move inputs
+        inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("Horizontal");
+        inputDirection.z = Input.GetAxis("Vertical");
+        if (inputDirection != Vector3.zero)
+        {
+            transform.forward = inputDirection;
+        }
+
+        // Get jump inputs
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+            rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+
+        // Get dash inputs
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward
+                , dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)
+                , 0
+                , (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
+            rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);        
+    }
+
+    #region Helper Methods
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+
+#if UNITY_EDITOR
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f)
+            , transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
+#endif
+
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down
+            , out hitInfo, groundCheckDistance, groundLayerMask))
+            isGrounded = true;
+        else
+            isGrounded = false;
+    }    
+    #endregion Helper Methods
+}
