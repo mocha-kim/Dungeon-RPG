0b1c7037c85fcf135ba05cff0bf423eec0c32f25
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-12-23 21:23:09 +0900

    [add] field of view

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    #region Variables
+
+    public float viewRadius = 5f;
+
+    [Range(0, 360)]
+    public float viewAngle = 90f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    private List<Transform> visibleTargets = new List<Transform>();
+    private Transform nearestTarget;
+    private float distanceToTarget;
+
+    public List<Transform> VisibleTargets => visibleTargets;
+
+    #endregion Variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FindVisibleTargets()
+    {
+        nearestTarget = null;
+        distanceToTarget = 0.0f;
+        visibleTargets.Clear();
+
+        // Find Targets in ViewRadius
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                // Find Targets in View
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                    // Find nearest Target
+                    if (nearestTarget == null || (distanceToTarget > dstToTarget))
+                    {
+                        nearestTarget = target;
+                        distanceToTarget = dstToTarget;
+                    }
+                }
+
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDeg, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+            angleInDeg += transform.eulerAngles.y;
+
+        return new Vector3(Mathf.Sin(angleInDeg / 2 * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDeg / 2 * Mathf.Deg2Rad));
+    }
+}
