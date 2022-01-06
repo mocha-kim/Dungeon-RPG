fa59553cb7f8a9d102bc1447c4f0ff0c7e503f34
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-01-06 17:59:31 +0900

    [add] camera obstacles vision

--- Assets/Scripts/Cameras/BackCamera.cs
+++ Assets/Scripts/Cameras/BackCamera.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class BackCamera : MonoBehaviour
 {
     #region Variables
 
-    public float height = 5f;
-    public float distance = 10f;
+    public float height = 1f;
+    public float distance = 3f;
     public float lookAtHeight = 2f;
     public float smoothSpeed = 0.5f;
 
+    private Vector3 boxPosition;
+    public Vector3 boxSize = new Vector3(4, 2, 2);
+
     public Transform target;
+
+    public LayerMask obstacleMask;
 
     private Vector3 refVelocity;
 
@@ -25,6 +31,26 @@ public class BackCamera : MonoBehaviour
     private void LateUpdate()
     {
         HandleCamera();
+
+        boxPosition = transform.position;
+        boxPosition.z += 2f;
+
+        Collider[] targets = Physics.OverlapBox(boxPosition, boxSize * 0.5f, transform.rotation, obstacleMask);
+
+        foreach (Collider target in targets)
+        {
+            StartCoroutine(disableRayHitObject(target.gameObject));
+        }
+    }
+
+    IEnumerator disableRayHitObject(GameObject go)
+    {
+        if (go.activeSelf)
+            go.SetActive(false);
+
+        yield return new WaitForSeconds(1.0f);
+
+        go.SetActive(true);
     }
 
     public void HandleCamera()
@@ -32,18 +58,22 @@ public class BackCamera : MonoBehaviour
         if (!target) return;
 
         // Calc world position vector
-        Vector3 worldPosition = (Vector3.forward * -distance) + (Vector3.up * height);
+        Vector3 worldPosition = (Vector3.forward * distance) + (Vector3.up * height);
         //Debug.DrawLine(target.position, worldPosition, Color.red);
+
+        // Calc rotate vector
+        Vector3 rotatedVector = Quaternion.AngleAxis(180f, Vector3.up) * worldPosition;
+        //Debug.DrawLine(target.position, rotatedVector, Color.green);
 
         // Move camera position
         Vector3 flatTargetPosition = target.position;
         flatTargetPosition.y += lookAtHeight;
 
-        Vector3 finalPosition = flatTargetPosition;
+        Vector3 finalPosition = flatTargetPosition + rotatedVector;
         //Debug.DrawLine(target.position, finalPosition, Color.blue);
 
         transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, smoothSpeed);
-        transform.LookAt(target.transform);
+        transform.LookAt(flatTargetPosition);
     }
 
     private void OnDrawGizmos()
@@ -57,5 +87,8 @@ public class BackCamera : MonoBehaviour
             Gizmos.DrawSphere(lookAtPosition, 0.25f);
         }
         Gizmos.DrawSphere(transform.position, 0.25f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxPosition, boxSize);
     }
 }
